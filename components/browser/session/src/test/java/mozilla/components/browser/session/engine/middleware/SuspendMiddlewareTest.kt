1a15f528f384b928a88eb5fc63a108b53b05194d
Christian Sadilek <christian.sadilek@gmail.com>
2020-08-07 11:07:03 -0400

    Add tests for TabsRemovedMiddleware

--- components/browser/session/src/test/java/mozilla/components/browser/session/engine/middleware/SuspendMiddlewareTest.kt
+++ components/browser/session/src/test/java/mozilla/components/browser/session/engine/middleware/SuspendMiddlewareTest.kt
@@ -5,9 +5,9 @@
 package mozilla.components.browser.session.engine.middleware
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.runBlocking
 import mozilla.components.browser.state.action.EngineAction
 import mozilla.components.browser.state.selector.findTab
 import mozilla.components.browser.state.state.BrowserState
@@ -18,9 +18,11 @@ import mozilla.components.concept.engine.EngineSessionState
 import mozilla.components.support.test.ext.joinBlocking
 import mozilla.components.support.test.libstate.ext.waitUntilIdle
 import mozilla.components.support.test.mock
+import mozilla.components.support.test.rule.MainCoroutineRule
 import mozilla.components.support.test.whenever
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.never
@@ -30,9 +32,12 @@ import org.mockito.Mockito.verify
 @RunWith(AndroidJUnit4::class)
 class SuspendMiddlewareTest {
 
+    @get:Rule
+    val coroutinesTestRule = MainCoroutineRule()
+
     @Test
-    fun `suspends engine session`() {
-        val scope = CoroutineScope(Dispatchers.IO)
+    fun `suspends engine session`() = runBlocking {
+        val scope = MainScope()
         val middleware = SuspendMiddleware(scope)
 
         val tab = createTab("https://www.mozilla.org", id = "1")
@@ -50,17 +55,15 @@ class SuspendMiddlewareTest {
         verify(engineSession).saveState()
 
         store.waitUntilIdle()
+        scope.coroutineContext[Job]?.children?.forEach { it.join() }
         assertNull(store.state.findTab(tab.id)?.engineState?.engineSession)
         assertEquals(state, store.state.findTab(tab.id)?.engineState?.engineSessionState)
-
-        scope.coroutineContext[Job]?.children?.forEach { it.joinBlocking() }
         verify(engineSession).close()
     }
 
     @Test
     fun `does nothing if tab doesn't exist`() {
-        val scope = CoroutineScope(Dispatchers.IO)
-        val middleware = SuspendMiddleware(scope)
+        val middleware = SuspendMiddleware(MainScope())
 
         val store = spy(BrowserStore(
             initialState = BrowserState(tabs = listOf()),
@@ -73,8 +76,7 @@ class SuspendMiddlewareTest {
 
     @Test
     fun `does nothing if engine session doesn't exist`() {
-        val scope = CoroutineScope(Dispatchers.IO)
-        val middleware = SuspendMiddleware(scope)
+        val middleware = SuspendMiddleware(MainScope())
 
         val tab = createTab("https://www.mozilla.org", id = "1")
         val store = spy(BrowserStore(
